33574a6ec27eb4908c7e43ca581d7cebb2383b92
Anwender <Anwender@DESKTOP-HT03I2D.localdomain>
2025-11-17 19:20:18 +0100

    Samir Test

--- RobOHMobil-Follower/Core/Src/main.c
+++ RobOHMobil-Follower/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+//Samir
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
